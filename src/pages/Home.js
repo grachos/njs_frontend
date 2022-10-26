b793376506df779ddeb9e368816d8c2f7950249f
grachos <graziany@gmail.com>
2022-10-26 17:43:16 -0500

    Commit: redireccionar caundo no esta logueado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,35 @@
 import React from "react";
-//import { useNavigate } from "react-router-dom";
+import {
+    Navigate,
+    //Link,
+    useNavigate
+} from "react-router-dom";
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
+
 
 const Home = () => {
-    /*const navegador = useNavigate("user");
-    //const idUsuario = localStorage.getItem();
-     if (idUsuario ===null){
-        //navegador("/");
-     }*/
-    return (
-        <div>
-            <Navbar />
-            <main className="flex-shrink-0">
-                <div className="container">
-                    <h1 className="mt-5">Página principal ServiPlus.</h1>
-                    <p className="lead">Sistema integrado de tickets que le permitirá registrar y hacer seguimiento a sus tickets. </p>
+    const idUsuario = localStorage.getItem("user");
+    if (idUsuario === null) {
+        return (
+            <Navigate to="/" replace state={"state"} />
+        );
+    } else {
+        return (
+            <div>
+                <Navbar />
+                <main className="flex-shrink-0">
+                    <div className="container">
+                        <h1 className="mt-5">Página principal ServiPlus.</h1>
+                        <p className="lead">Sistema integrado de tickets que le permitirá registrar y hacer seguimiento a sus tickets. </p>
                     </div>
-            </main>
+                </main>
 
-            <Footer />
-        </div>
-    );
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Home;
